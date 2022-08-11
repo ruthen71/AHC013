14652d3551dd3e178b42ff28b99dcb2ed82cddf0
ruthen71 <takahero2016@gmail.com>
2022-08-11 16:33:05 +0900

    first commit

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef _RUTHEN
+#include "debug.hpp"
+#else
+#define show(...) true
+#endif
+
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+template <class T> using V = vector<T>;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}
